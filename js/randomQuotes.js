fd02c071eecdf3532a0d241b46c1d6bfd39fbfc3
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-10 14:55:33 +0200

    Update randomQuotes.js

--- js/randomQuotes.js
+++ js/randomQuotes.js
@@ -12,23 +12,23 @@ function newRandomQuote(){
     let quotes=[
      {
        text:"Essen und Trinken hält Leib und Seele zusammen",
-       source:"Sokrates"
+       source:"  -Sokrates"
      },
      {
        text:"Das Wort der Wahrheit lautet schlicht und ungeschminkt",
-       source:"Euripides"
+       source:"  -Euripides"
      },
      { 
        text:"Jeder Tag ist ein Wagnis und wird dadurch erst lebenswert",
-       source:"Aristoteles"
+       source:"  -Aristoteles"
      },
      { 
        text:"Der Charakter ist das Schicksal des Menschen",
-       source:"Heraklit"
+       source:"  -Heraklit"
      },
      {
        text:"Manches können die Menschen besser, und manches die Skunks",
-       source:"Mephitis"
+       source:"  -Mephitis"
      }];
      let colors=['#e6ac00',"#ff704d",'#0099ff','#33cc00','#aa80ff','#ffa31a'];
      let randQuote = quotes[Math.floor(Math.random()*(quotes.length))];
