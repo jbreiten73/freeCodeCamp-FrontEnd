0091b9890c0f1e6a4dc7f9db9672faa7e7733a01
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-10 14:28:46 +0200

    Update randomQuotes.js

--- js/randomQuotes.js
+++ js/randomQuotes.js
@@ -31,8 +31,13 @@ function newRandomQuote(){
        source:"Mephitis"
      },
      ];
-
+     let colors=['#e6ac00',"#ff704d",'#0099ff",
+     '#33cc00','#aa80ff','#ffa31a'];
      let randQuote = quotes[Math.floor(Math.random()*(quotes.length))];
+     let randColor = colors[Math.floor(Math.random()*(colors.length))];
      $("#text").html(randQuote.text);
+     $("#text").css("color",randColor);
      $("#author").html(randQuote.source);
+     $("#author").css("color",randColor);
+     $("body").css("background-color",randColor);
 }
