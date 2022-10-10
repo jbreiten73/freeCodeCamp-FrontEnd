d2de648751da3fea822e3807ae10fd3fac68c753
jbreiten73 <julia.breitenbruch@gmail.com>
2022-10-10 17:50:53 +0200

    Update randomQuotes.js

--- js/randomQuotes.js
+++ js/randomQuotes.js
@@ -64,6 +64,7 @@ function newRandomQuote(){
      $("#text").html(randQuote.text);
      $("#text").css("color","#ffffff");
      $("#text").css("font-size","72px");
+     $("#text").css("font-family","serif");
      $("#author").html(randQuote.source);
      $("#author").css("color",#ffffff);
      $("#author").css("font-size","32px");
